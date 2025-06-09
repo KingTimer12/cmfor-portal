6166a6c86b41e15cffdeeeb3369bad53f19e08b3
unknown <queentimer12@gmail.com>
2025-06-08 23:26:44 -0300

    first commit

---
+++ settings/index.js
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+    APP_NAME: z.string().min(1, "APP_NAME is required"),
+	API_URL: z.string().url(),
+});
+
+const env = envSchema.parse({
+	API_URL: process.env.EXPO_PUBLIC_API_URL,
+    APP_NAME: process.env.EXPO_PUBLIC_APP_NAME,
+});
+
+export const settings = {
+	...env,
+};
