b3a2f6c2a5ff7a6d0c443b67f573301c6f2ab2e1
Aaron King <queentimer12@gmail.com>
2025-06-23 08:12:18 -0300

    chore: otimizado a renderização das imagens

--- components/_slider.jsx
+++ components/_slider.jsx
@@ -6,7 +6,7 @@ const { width } = Dimensions.get('screen');
 const SliderItem = ({ item }) => {
     return (
         <View className='flex flex-col items-center justify-center p-4 gap-5' style={{ width: width }}>
-            <Image source={{ uri: item.image }} style={{ width: 450, height: 250 }} resizeMethod='scale' resizeMode='center' />
+            <Image source={{ uri: item.image }} style={{ width: 450, height: 250 }} resizeMode='center' />
             <View className='absolute p-5 rounded-md bottom-6 left-11 right-0 shadow-lg bg-white/90 w-96'>
                 <Text className='text-primary text-xl font-bold' numberOfLines={2} ellipsizeMode='tail'>{item.title}</Text>
             </View>
